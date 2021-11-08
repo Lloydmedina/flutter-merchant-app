bcc8f129b3f472fcc43cfa798e54cdf32cad761b
Lloydmedina <79982709+Lloydmedina@users.noreply.github.com>
2021-11-08 16:00:06 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,71 +25,12 @@ class _MyAppState extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
       getPages: [
         GetPage(
-            name: '/',
-            page: () {
-              return box.hasData("accessToken") ? HomeView() : LoginView();
-            },
-         //   binding: InitialBinding()),
-        //GetPage(
-         //   name: "/dash", page: () => LoginView(), binding: LoginBinding()),
-        // GetPage(
-        //     name: "/login", page: () => LoginView(), binding: LoginBinding()),
-        // GetPage(
-        //     name: "/sign_up_initial_checking",
-        //     page: () => RegistrationInitialChecking(),
-        //     binding: ForgotPasswordBinding()),
-        // GetPage(
-        //     name: "/sign_up_initial_checking/verify",
-        //     page: () => VerifyInitialCheckingView(),
-        //     binding: ForgotPasswordBinding()),
-        // GetPage(
-        //     name: "/sign_up",
-        //     page: () => RegistrationGetStartedView(),
-        //     binding: RegistrationBinding()),
-        // GetPage(
-        //     name: "/sign_up/basic_information",
-        //     page: () => RegistrationBasicInformationView(),
-        //     binding: RegistrationBinding()),
-        // GetPage(
-        //     name: "/sign_up/photo_identification",
-        //     page: () => RegistrationPhotoIdentificationView(),
-        //     binding: RegistrationBinding()),
-        // GetPage(
-        //     name: "/sign_up/supporting_documents",
-        //     page: () => RegistrationSupportingDocumentView(),
-        //     binding: RegistrationBinding()),
-        // GetPage(
-        //     name: "/sign_up/vehicle_information",
-        //     page: () => RegistrationVehicleInformationView(),
-        //     binding: RegistrationBinding()),
-        // GetPage(name: "/success", page: () => SuccessView()),
-        // GetPage(
-        //     name: "/forgot_password/set_new_password",
-        //     page: () => SetNewPassword()),
-        // GetPage(
-        //     name: "/forgot_password",
-        //     page: () => ForgotPasswordView(),
-        //     binding: ForgotPasswordBinding()),
-        // GetPage(name: "/otp", page: () => OtpView()),
-        // GetPage(
-        //     name: "/otpPage",
-        //     page: () => OtpPage("639394300929"),
-        //     binding: ForgotPasswordBinding()),
-        // GetPage(
-        //     name: "/set_new_password",
-        //     page: () => SetNewPassword(),
-        //     binding: ForgotPasswordBinding()),
-        // GetPage(name: "/home", page: () => DashBoardView()),
-        // GetPage(name: "/account", page: () => AccountView()),
-        // GetPage(name: "/account/detail", page: () => DriverAccountDetailView()),
-        // GetPage(
-        //     name: "/requirement", page: () => RequirementMainSelectorView()),
-        // GetPage(name: "/wallet", page: () => WalletView()),
-        // GetPage(name: "/wallet_new", page: () => WalletHistoryView()),
-        // GetPage(name: "/earning", page: () => AccountView()),
-        // GetPage(name: "/topuphistory", page: () => TopUpHistoryDetails()),
+          name: '/',
+          page: () {
+            return box.hasData("accessToken") ? HomeView() : LoginView();
+          },
+        ),
         GetPage(name: "/home", page: () => HomeView()),
-        ) 
       ],
       initialRoute: '/',
     );
